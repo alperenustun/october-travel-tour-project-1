9398814771ed19717fef35bd3eaa206a2c877a5e
Meth <122786519+MethRAa@users.noreply.github.com>
2023-11-11 00:25:58 +0300

    [OC-67] Create-Package-Modal tamamlandı.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const dropDownLinks = document.getElementById("drop-down-links");
 const servicesLink = document.getElementById("services-link");
+const owerlayPopup = document.getElementById("homepage-hero-ower");
+const contactOwerlay = document.getElementById("contact-owerlay");
+const closeOwerlay = document.getElementById("owerlay-close-btn");
+const scrollOff = document.body;
 
 let isModalOpen = false;
 
@@ -11,4 +15,12 @@ servicesLink.addEventListener("click", () => {
     dropDownLinks.style.display = "none";
     isModalOpen = !isModalOpen;
   }
+});
+function increment() {
+  scrollOff.style.overflowY = "hidden";
+  owerlayPopup.style.display = "block";
+}
+closeOwerlay.addEventListener("click", () => {
+  scrollOff.style.overflowY = "scroll";
+  owerlayPopup.style.display = "none";
 });
