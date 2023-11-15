074e0e11c56a32319a0a4450f921c9340e821c5e
Meth <122786519+MethRAa@users.noreply.github.com>
2023-11-15 14:44:11 +0300

    [OC-68] son düzenlemeler yapıldı.

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const contactOwerlay = document.getElementById("contact-overlay");
 const closeOverlay = document.getElementById("overlay-close-btn");
 const navBarPositions = document.getElementById("hero-navbar");
 const scrollOff = document.body;
+const locationNavbarBoxes = document.querySelectorAll(".location-navbar-box");
+
+for (let i = 0; i < locationNavbarBoxes.length; i++) {
+  locationNavbarBoxes[i].addEventListener("click", () => {
+    for (let j = 0; j < locationNavbarBoxes.length; j++) {
+      locationNavbarBoxes[j].style.backgroundColor = "#f8f8f8";
+    }
+    locationNavbarBoxes[i].style.backgroundColor = "#fff";
+  });
+}
 
 let isModalOpen = false;
 
