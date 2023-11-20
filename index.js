25d73c73e02c5ce1057149f76aad2139c0829445
alperenustun <alperen.ustun39@gmail.com>
2023-11-20 13:57:08 +0300

    update

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const closeOverlay = document.getElementById("overlay-close-btn");
 const navBarPositions = document.getElementById("hero-navbar");
 const scrollOff = document.body;
 const locationNavbarBoxes = document.querySelectorAll(".location-navbar-box");
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const navbar = document.querySelector(".nav-links-box");
 
 for (let i = 0; i < locationNavbarBoxes.length; i++) {
   locationNavbarBoxes[i].addEventListener("click", () => {
@@ -37,6 +39,19 @@ closeOverlay.addEventListener("click", () => {
   overlayPopup.style.display = "none";
   navBarPositions.style.position = "static";
 });
+
+
+let isNavbarOpen = false;
+
+hamburgerIcon.addEventListener("click", () => {
+  if(!isNavbarOpen){
+    navbar.style.display = "flex";
+    isNavbarOpen = !isNavbarOpen;
+  } else {
+    navbar.style.display = "none";
+    isNavbarOpen = !isNavbarOpen;
+  }
+})
 
 // const dateSelection = document.querySelector(".inputs-box-date");
 // dateSelection.addEventListener("change", (e) => {
